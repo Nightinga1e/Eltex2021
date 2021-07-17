9eed84a83c64338cc37df83b3ad0b1d7f9d90546
Nightinga1e <smaximd@gmail.com>
2021-07-18 06:26:13 +0700

    make some double\float tests

--- 0. additional/FloatVar.c
+++ 0. additional/FloatVar.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-float sum(float test){
+float sumfloat(float test){
 	float temp = 0.0;
 	for (int i=0; ;i++)
 	{
@@ -15,17 +15,40 @@ float sum(float test){
 	return test;
 }
 
+double sumdouble(double test){
+	double temp = 0.0;
+	for (int i=0; ;i++)
+	{
+		temp = test;
+		test += 1.0;
+		/*if (temp == test)
+		{
+			printf("%f \n", test);
+			break;
+		}*/
+		if (i>16777224)
+		{
+			printf("%f \n", test);
+		}
+	}
+	return test;
+}
 
 int main()
 {
 	float test = 0.0;
-	printf(" First test \n");		
-	test = sum(test);
+	printf(" First float test \n");		
+	test = sumfloat(test);
 	printf(" Second test \n");
-	test = test + 2;
-	test = sum(test);
+	test = test + 2.0;
+	test = sumfloat(test);
 	printf(" Third test \n");
-	test = test + 2;
-	test = sum(test);
+	test = test + 2.0;
+	test = sumfloat(test);
+	
+	double test2 = 0.0;
+	printf(" First double test \n");
+	test2 = sumdouble(test2);
+	
 	return 0;
 }
