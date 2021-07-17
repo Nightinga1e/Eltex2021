f2fe296f5417afb5f87d6abf677f508badc36593
Nightinga1e <smaximd@gmail.com>
2021-07-18 05:40:50 +0700

    Made some float tests

---
+++ 0. additional/FloatVar.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+float sum(float test){
+	float temp = 0.0;
+	for (int i=0; ;i++)
+	{
+		temp = test;
+		test += 1.0;
+		if (temp == test){
+			printf("%f \n", test);
+			break;
+		}
+	}
+	return test;
+}
+int main()
+{
+	float test = 0.0;
+	printf(" First test \n");		
+	test = sum(test);
+	printf(" Second test \n");
+	test = test + 2;
+	test = sum(test);
+	printf(" Third test \n");
+	test = test + 2;
+	test = sum(test);
+	return 0;
+}
