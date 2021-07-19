565b8a3680e628bd1d4e2b9481e5242f34fd9853
Nightinga1e <smaximd@gmail.com>
2021-07-19 21:17:22 +0700

    Add another way to change variable

--- 0. additional/ByteChange.c
+++ 0. additional/ByteChange.c
@@ -3,8 +3,8 @@
 int main(){
 	int a = 0x00DDAABB;
 	int c = 0xCC;
-	printf("c = %x \n", c);
 	printf("a = %x \n", a);
+	printf("c = %x \n", c);
 
 	char *ptra;
 	ptra = (char*)&a;
@@ -12,5 +12,13 @@ int main(){
 	*ptra = c;
 	printf("*ptra = c = %x \n", *ptra);
 	printf("result a = %x \n", a); 
+	
+	a = 0x00DDAABB;
+	c = 0x00110000;
+	printf("\na = %x \n", a);
+	printf("c = %x \n", c);
+
+	a = a-c;
+	printf("result a = %x \n", a);
 	return 0;
 }
