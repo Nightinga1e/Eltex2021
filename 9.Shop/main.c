1bc0dba34078ddc412ccdf2255b6d0e603ee76e5
Nightinga1e <smaximd@gmail.com>
2021-08-14 03:17:11 +0700

    Worker and customers threads created

--- 9.Shop/main.c
+++ 9.Shop/main.c
@@ -5,15 +5,41 @@
 #include <pthread.h>
 
 #define NUMOFSHOPS 5
+#define NUMOFCUSTOMERS 3
+
+void *custbuy(void *args)
+{
+	printf("\n I am customer\n");
+}
+
+void *workerload(void *args)
+{
+	printf("\nI am worker\n");
+}
 
 int main()
 {
 	int shops[NUMOFSHOPS];
-	
+	pthread_t customers[NUMOFCUSTOMERS];
+	pthread_t worker;
+
 	for (int i = 0; i < NUMOFSHOPS; i++)
 	{
 		shops[i] = 900 + rand() % 200;
 		printf("\nShop #%d have %d items\n", i+1, shops[i]); 
 	}
+
+	for (int i =0; i< NUMOFCUSTOMERS; i++)
+	{
+		pthread_create(&customers[i], NULL, custbuy, NULL);
+	}
+	pthread_create(&worker, NULL, workerload, NULL);
+	
+	for (int i =0; i< NUMOFCUSTOMERS; i++)
+	{
+		pthread_join(customers[i], NULL);
+	}
+	pthread_join(worker, NULL);
+
 	return 0;
 }
